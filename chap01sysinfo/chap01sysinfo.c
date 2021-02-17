16cb03a1adb50c7ccf2c74b8b95e7b679b882d3f
Hecarim7 <hyoung7866@gmail.com>
2021-02-17 15:03:40 +0900

    chap01 sizeof

---
+++ chap01sysinfo/chap01sysinfo.c
@@ -0,0 +1,27 @@
+﻿
+
+
+#include <stdio.h>
+
+int main()
+{
+	printf(">>> system information <<<\n");
+
+	printf("sizeof(char)(%d)\n", sizeof(char));
+	printf("sizeof(short)(%d)\n", sizeof(short));
+	printf("sizeof(int)(%d)\n", sizeof(int));
+	printf("sizeof(long)(%d)\n", sizeof(long));
+	printf("sizeof(float)(%d)\n", sizeof(float));
+	printf("sizeof(double)(%d)\n", sizeof(double));
+
+	printf("sizeof(char*)(%d)\n", sizeof(char*));
+	printf("sizeof(short*)(%d)\n", sizeof(short*));
+	printf("sizeof(int*)(%d)\n", sizeof(int*));
+	printf("sizeof(long*)(%d)\n", sizeof(long*));
+	printf("sizeof(float*)(%d)\n", sizeof(float*));
+	printf("sizeof(double*)(%d)\n", sizeof(double*));
+
+	printf("sizeof(void*)(%d)\n", sizeof(void*));
+
+		
+}
